6143bf243aae9915fb9bfd8a4da1641f563f76d5
dashixiong <876337269@qq.com>
2021-08-31 11:47:48 +0800

    fix: 修复sass引用全局scss文件

--- packages/vmi/src/plugins/component.ts
+++ packages/vmi/src/plugins/component.ts
@@ -67,19 +67,17 @@ export default (api: IApi) => {
 
     memo.externals(externals);
 
-    // 添加全局scss文件
-    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+    // todo 暂时只对component打包生效，后续project的时候需要移到公共逻辑
+    const oneOfsMap = memo.module.rule('sass').oneOfs.values();
 
-    types.forEach((type) => {
-      //匹配到所有需要导入的文件
-      memo.module
-        .rule('sass')
-        .oneOf(type)
-        .use('style-resource')
-        .loader('style-resources-loader')
+    oneOfsMap.forEach((item) => {
+      item
+        .use('style-resources-loader')
+        .loader(require.resolve('style-resources-loader'))
         .options({
-          patterns: [require.resolve('@winfe/theme-helper')],
-        });
+          patterns: require.resolve('@winfe/theme-helper'),
+        })
+        .end();
     });
 
     memo
